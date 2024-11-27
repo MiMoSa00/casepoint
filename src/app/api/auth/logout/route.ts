22df8deb0cfd819db09c9cdc62f9805c2f6a2361
Oluwamomi <oluwamomiokpaise@gmail.com>
2024-11-26 19:14:46 -0800

    except

--- src/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from 'next/server';
-// import { useKindeAuth } from '@kinde-oss/kinde-auth-nextjs';
-
-// export async function GET() {
-//   const logoutUrl = process.env.NEXT_PUBLIC_KINDE_ISSUER_URL + '/oauth2/v1/logout';
-//   return NextResponse.redirect(logoutUrl);
-// }
